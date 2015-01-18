c818cb637f3e42fd4e96a887b05d95f22ab0f46f
Brian <iamamthwiz+GitHub@gmail.com>
2015-01-17 19:24:39 -0800

    convert json date/time to JS date object

--- js/models/data_point.js
+++ js/models/data_point.js
@@ -1,2 +1,12 @@
 DVD.Models.DataPoints = Backbone.Model.extend({
+    parse: function (jsonResponse) {
+        if (jsonResponse.date && jsonResponse.time) {
+            var dateString = jsonResponse.date + " " + jsonResponse.time;
+            jsonResponse.date = new Date(dateString);
+            
+            delete jsonResponse.time;
+        }
+        
+        return jsonResponse;
+    }
 });
