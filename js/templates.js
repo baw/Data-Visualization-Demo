384e8dac8ff227648c2e176e20e1455353c2c1a2
Brian <iamamthwiz+GitHub@gmail.com>
2015-01-22 22:49:13 -0800

    add minified source

---
+++ js/templates.js
@@ -0,0 +1,31 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["line_chart_view"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<div id="chart"></div>\n<div><button id="trendlineToggle">Add Trendline</button></div>';
+
+}
+return __p
+};
+
+this["JST"]["main_view"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<section id="day_buttons">\n  <ul class="buttons">\n    <li><button id="today">Today</button></li>\n    <li><button id="three_days">3 Days</button></li>\n    <li><button id="seven_days">7 Days</button></li>\n    <li><button id="fifteen_days" class="selected">15 Days</button></li>\n  </ul>\n</section>\n\n<section id="line_chart"></section>\n\n<section id="segments" class="left"></section>\n\n<section id="pie_chart" class="right"></section>';
+
+}
+return __p
+};
+
+this["JST"]["segments"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<h2>Segments</h2>\n<section>\n  <div><button id="all_button" class="selected">All:</button> <span id="total_count">0</span></div>\n  <div><button id="male_button">Male:</button> <span id="male_count">0</span></div>\n  <div><button id="female_button">Female:</button> <span id="female_count">0</span></div>\n</section>';
+
+}
+return __p
+};
