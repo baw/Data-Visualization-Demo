9713d544ae810f9f6b572739b6fa46c20ad16bcd
Brian <iamamthwiz+GitHub@gmail.com>
2015-01-22 22:47:29 -0800

    add grunt build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,17 +4,37 @@ module.exports = function (grunt) {
             compile: {
                 options: {
                     processName: function (filePath) {
-                       return filePath.replace("js/templates/", "").replace(".jst", ""); 
+                       return filePath.replace("js/templates/", "").replace(".jst", "");
                     }
                 },
                 files: {
                     "js/templates.js" : ["js/templates/*.jst"]
                 }
             }
+        },
+        uglify: {
+            options: {
+                mangle: {
+                    except: ["$", "Backbone", "_", "DVD"]
+                }
+            },
+            my_target: {
+                files: {
+                    "js/source.min.js": ["js/main.js",
+                                      "js/utl/*.js",
+                                      "js/models/*.js",
+                                      "js/collections/*.js",
+                                      "js/routers/*.js",
+                                      "js/templates.js",
+                                      "js/views/*.js"]
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks("grunt-contrib-jst");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
 
     grunt.registerTask("precompile", ["jst"]);
+    grunt.registerTask("build", ["jst", "uglify"]);
 };
