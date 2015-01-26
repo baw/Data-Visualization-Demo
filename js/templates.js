98c333e910cceaac5af50e45f2d6ec2b9da81e31
Brian <iamamthwiz+GitHub@gmail.com>
2015-01-26 14:06:44 -0800

    rebuild

--- js/templates.js
+++ js/templates.js
@@ -14,7 +14,17 @@ this["JST"]["main_view"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<section id="day_buttons">\n  <ul class="buttons">\n    <li><button id="today">Today</button></li>\n    <li><button id="three_days">3 Days</button></li>\n    <li><button id="seven_days">7 Days</button></li>\n    <li><button id="fifteen_days" class="selected">15 Days</button></li>\n  </ul>\n</section>\n\n<section id="line_chart"></section>\n\n<section id="segments" class="left"></section>\n\n<section id="pie_chart" class="right"></section>';
+__p += '<section id="day_buttons">\n  <ul class="buttons">\n    <li><button id="today">Today</button></li>\n    <li><button id="three_days">3 Days</button></li>\n    <li><button id="seven_days">7 Days</button></li>\n    <li><button id="fifteen_days" class="selected">15 Days</button></li>\n  </ul>\n</section>\n\n<section id="line_chart"></section>\n\n<section id="segments" class="left"></section>\n\n<section id="pie_chart" class="right"></section>\n\n<section id="map_view" class="map_view"></section>';
+
+}
+return __p
+};
+
+this["JST"]["map_view"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<h2>Rate of activity</h2>\n<div id="map" class="map"></div>';
 
 }
 return __p
